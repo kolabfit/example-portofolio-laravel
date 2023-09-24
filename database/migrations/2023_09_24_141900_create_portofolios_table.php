a71322caf27a7a82cedd8aaeba4150945e313cc5
Andri Junaedi <andri@sawala.tech>
2023-09-24 21:24:10 +0700

    feat: add migrations table kategories, portofolios, pegawais

---
+++ database/migrations/2023_09_24_141900_create_portofolios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portofolios', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('kategori_id');
+            $table->string('judul_porto');
+            $table->integer('tahun');
+            $table->text('deskripsi');
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portofolios');
+    }
+};
