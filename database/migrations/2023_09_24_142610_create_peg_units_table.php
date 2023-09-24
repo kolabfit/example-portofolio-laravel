809ff471bc811787c7ee369a7c4f314c5862a67d
Andri Junaedi <andri@sawala.tech>
2023-09-24 21:27:27 +0700

    feat: add migrations table direktorats and peg_units

---
+++ database/migrations/2023_09_24_142610_create_peg_units_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peg_units', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('pegawai_id');
+            $table->unsignedInteger('direktorats_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peg_units');
+    }
+};
